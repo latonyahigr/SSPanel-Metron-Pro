cfe17b13ce884022a6a9bc068ed9eed838590887
embrace <123@163.com>
2023-06-27 23:07:56 +0800

    ui调整

--- storage/framework/smarty/compile/641732412d0275eafb118f04ea484f561f0b865c_0.file.scripts.tpl.php
+++ storage/framework/smarty/compile/641732412d0275eafb118f04ea484f561f0b865c_0.file.scripts.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.48, created on 2023-06-24 23:53:05
+/* Smarty version 3.1.48, created on 2023-06-27 23:07:36
   from 'D:\XProject\SSPanel\resources\views\metron\include\auth\scripts.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.48',
-  'unifunc' => 'content_64971161e1ae83_40368106',
+  'unifunc' => 'content_649afb38a5bb97_14973758',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '641732412d0275eafb118f04ea484f561f0b865c' => 
     array (
       0 => 'D:\\XProject\\SSPanel\\resources\\views\\metron\\include\\auth\\scripts.tpl',
-      1 => 1686844839,
+      1 => 1687878454,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:include/global/chatra.tpl' => 1,
   ),
 ),false)) {
-function content_64971161e1ae83_40368106 (Smarty_Internal_Template $_smarty_tpl) {
+function content_649afb38a5bb97_14973758 (Smarty_Internal_Template $_smarty_tpl) {
 ?>                        <div class="p-5" id="auth-stf"></div>
                     </div>
                 </div>
@@ -110,6 +110,35 @@ function content_64971161e1ae83_40368106 (Smarty_Internal_Template $_smarty_tpl)
         <?php $_smarty_tpl->_subTemplateRender('file:include/global/chatra.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
         <?php }?>
-        <?php }
-}
+        <?php }?>
+
+                        <?php echo '<script'; ?>
+>
+                            window.onload = function (){
+                                function getMessage(){
+                                    // 获取当前时间
+                                    let timeNow = new Date();
+                                    // 获取当前小时
+                                    let hours = timeNow.getHours();
+                                    // 设置默认文字
+                                    let text = ``;
+                                    // 判断当前时间段
+                                    if (hours >= 0 && hours <= 10) {
+                                        text = `Good Morning`;
+                                    } else if (hours > 10 && hours <= 14) {
+                                        text = `Good Afternoon`;
+                                    } else if (hours > 14 && hours <= 18) {
+                                        text = `Good Afternoon`;
+                                    } else if (hours > 18 && hours <= 24) {
+                                        text = `Good Evening`;
+                                    }
+                                    // 返回当前时间段对应的状态
+                                    return text;
+                                }
+                                document.getElementsByClassName('message')[0].innerHTML = getMessage();
+                            }
+
+
+                        <?php echo '</script'; ?>
+><?php }
 }
